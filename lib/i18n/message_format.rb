3a3c58a9a43d9122a51cdb69510deb1046c56932
Chris Fung <aergonaut@gmail.com>
2026-02-25 18:23:43 -0800

    initial commit

---
+++ lib/i18n/message_format.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "message_format/version"
+
+module I18n
+  module MessageFormat
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
